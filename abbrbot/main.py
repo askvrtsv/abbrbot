1d8efbc6c019628b00a4b4b0e61d24a2a3f027e3
Anton Skvortsov <askvrtsv@icloud.com>
2024-02-14 10:03:07 +0300

    Use keyboard buttons instead of command args

--- abbrbot/main.py
+++ abbrbot/main.py
@@ -1,24 +1,30 @@
 import logging
 import os
+import pathlib
 import random
 
+import telegram
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-TOKEN = os.environ["TELEGRAM_TOKEN"]
-
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+from telegram.ext import (
+    ApplicationBuilder,
+    CallbackQueryHandler,
+    CommandHandler,
+    ContextTypes,
 )
 
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+TEMPLATES_DIR = pathlib.Path(__file__).resolve().parent / "templates"
 
-class AbbrException(Exception):
-    pass
+
+def setup_logging():
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+    )
 
 
-class InvalidArgument(AbbrException):
-    pass
+def load_template(template_path: pathlib.Path) -> str:
+    return template_path.open(mode="r", encoding="utf-8").read()
 
 
 def generate_abbr(length: int) -> str:
@@ -26,44 +32,57 @@ def generate_abbr(length: int) -> str:
     return "".join(random.choice(SYMBOLS) for _ in range(length))
 
 
-def parse_next_args(args: list) -> int:
-    args = list(args)
-    try:
-        length = int(args.pop(0))
-        if not (5 >= length >= 3):
-            raise InvalidArgument("Размер должен быть от 3 до 5")
-    except IndexError:
-        length = 3
-    except ValueError:
-        raise InvalidArgument("Размер должен быть числом")
-    return length
-
-
-async def next_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     assert update.effective_chat
-    assert update.message
-
-    try:
-        length = parse_next_args(context.args or [])
-    except InvalidArgument as exception:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            reply_to_message_id=update.message.id,
-            text=str(exception),
-        )
-        return None
 
     await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=generate_abbr(length),
+        update.effective_chat.id,
+        parse_mode=telegram.constants.ParseMode.MARKDOWN_V2,
+        text=load_template(TEMPLATES_DIR / "start.md"),
     )
 
 
-def run_application():
-    application = ApplicationBuilder().token(TOKEN).build()
-    application.add_handler(CommandHandler("next", next_command))
-    application.run_polling()
+async def handle_callback_query(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    assert update.callback_query
+
+    query = update.callback_query
+    await query.answer()
+
+    match query.data:
+        case None:
+            pass
+        case s if s.startswith("abbr-length"):
+            length = int(s.split(":")[-1])
+            await query.edit_message_text(generate_abbr(length))
+
+
+async def new_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    assert update.effective_chat
+    assert update.message
+
+    keyboard = [[
+        telegram.InlineKeyboardButton("3", callback_data="abbr-length:3"),
+        telegram.InlineKeyboardButton("4", callback_data="abbr-length:4"),
+        telegram.InlineKeyboardButton("5", callback_data="abbr-length:5"),
+    ]]
+    reply_markup = telegram.InlineKeyboardMarkup(keyboard)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Выберите размер:",
+        reply_markup=reply_markup,
+    )
+
+
+def run_app():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler(["start", "help"], start_command))
+    app.add_handler(CommandHandler("new", new_command))
+    app.add_handler(CallbackQueryHandler(handle_callback_query))
+    app.run_polling()
 
 
 if __name__ == "__main__":
-    run_application()
+    setup_logging()
+    run_app()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,12 @@
 from abbrbot.main import generate_abbr
 
 
-def test_abbr_size():
+def test_abbr_length():
     assert len(generate_abbr(3)) == 3
     assert len(generate_abbr(100)) == 100
     assert len(generate_abbr(-1)) == 0
 
 
-def test_abbr_value():
+def test_abbr_chars_are_upper():
     abbr = generate_abbr(3)
     assert abbr == abbr.upper()
